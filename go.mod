27a2a98ce8c6ba2ea42754b16d03384245a61454
noisersup <patryk@kwiatek.xyz>
2022-02-04 04:31:32 +0100

    add search bar

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/noisersup/ledyt
 
 go 1.16
 
-require github.com/charmbracelet/bubbletea v0.19.3
+require (
+	github.com/charmbracelet/bubbles v0.10.2 // indirect
+	github.com/charmbracelet/bubbletea v0.19.3
+	github.com/charmbracelet/lipgloss v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,16 +5,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	yt "github.com/noisersup/ledyt/yt-client"
 )
 
 func main() {
-	client := yt.Client{http.Client{}}
-	v, err := client.Search("minecraft")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//v := mockVideos(20)
+	v := []yt.Video{}
 
 	prog := tea.NewProgram(initialModel(v))
 	if err := prog.Start(); err != nil {
@@ -37,22 +36,33 @@ func mockVideos(n int) []yt.Video {
 }
 
 type model struct {
-	videos []yt.Video
-	cursor int
+	videos      []yt.Video
+	cursor      int
+	searchInput textinput.Model
+	client      *yt.Client
 }
 
 func initialModel(v []yt.Video) model {
+	searchInput := textinput.New()
+	searchInput.Placeholder = "Search..."
+	searchInput.CharLimit = 50 //TODO: search for max
+	searchInput.Width = 20
+
 	return model{
-		videos: v,
-		cursor: 0,
+		videos:      v,
+		cursor:      0,
+		searchInput: searchInput,
+		client:      &yt.Client{http.Client{}},
 	}
 }
 
 func (m model) Init() tea.Cmd {
-	return nil // no I/O
+	return tea.Batch(textinput.Blink, tea.EnterAltScreen)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	//key press
 	case tea.KeyMsg:
@@ -69,20 +79,68 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if m.cursor < len(m.videos)-1 {
 				m.cursor++
 			}
-		}
 
+		case "/":
+			m.searchInput.Focus()
+			return m, cmd
+		case tea.KeyEsc.String():
+			m.searchInput.Blur()
+
+		case tea.KeyEnter.String():
+			if m.searchInput.Focused() {
+				query := m.searchInput.Value()
+				m.searchInput.Blur()
+
+				v, err := m.client.Search(query)
+				if err != nil {
+					return m, cmd
+				}
+
+				m.videos = v
+			}
+		}
 	}
-	return m, nil
+
+	m.searchInput, cmd = m.searchInput.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
 	s := "[LEDYT]\n\n"
+	s += m.searchBar()
+	s += "\n"
+	s += m.searchList()
+	return s
+}
+
+func (m model) searchBar() string {
+	var s string
+	var borderStyle = lipgloss.NewStyle().
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("63")).
+		Width(25)
+
+	s += borderStyle.Render(m.searchInput.View())
+	return s
+}
+
+func (m model) searchList() string {
+	var s string
+	var style = lipgloss.NewStyle().
+		Bold(true).
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("63"))
+	var selectedStyle = lipgloss.NewStyle().Background(lipgloss.Color("63"))
+
 	for i, video := range m.videos {
 		cursor := " "
 		if m.cursor == i {
 			cursor = ">"
+			s += selectedStyle.Render(fmt.Sprintf("%s %s [author: %s]", cursor, video.Title, video.Channel.Name))
+			s += "\n"
+		} else {
+			s += fmt.Sprintf("%s %s [author: %s]\n", cursor, video.Title, video.Channel.Name)
 		}
-		s += fmt.Sprintf("%s %s [author: %s]\n", cursor, video.Title, video.Channel.Name)
 	}
-	return s
+	return style.Render(s)
 }
